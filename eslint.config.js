c4e7270a92f9835ebfe6834209eb854e0f611292
Fedor Indutny <238531+indutny@users.noreply.github.com>
2024-06-14 07:36:12 -0700

    chore: update to eslint@9, add ava

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import prettier from 'eslint-config-prettier';
+import ava from 'eslint-plugin-ava';
+
+export default {
+  plugins: {
+    prettier,
+    ava,
+  },
+  rules: {
+    'no-restricted-syntax': 'off',
+    'no-continue': 'off',
+    'class-methods-use-this': 'off',
+    'no-bitwise': 'error',
+    'no-plusplus': 'error',
+    yoda: 'off',
+  },
+  ignores: ['test.js'],
+};
